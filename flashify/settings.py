efb05ebe5471b5f6e597a994951e1dc026b91992
Yurii Bykov <jim95022@gmail.com>
2024-01-27 15:13:48 +0400

    extracted env value

--- flashify/settings.py
+++ flashify/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-awu7d^sjyki*qe*_5b+qv94n2j@fr-q&*amf=n@gu2t&a9=x@1"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
